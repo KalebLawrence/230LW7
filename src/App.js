339666c9a29a9a72831fd99e8cbebaa7b507c4a4
KalebLawrence <ksl00007@mix.wvu.edu>
2024-02-25 17:05:35 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>CS 230L</h1>
+    <h2>Section - 02</h2>
+    <p>WVU ID: 800385575</p>
+    <p>Hi, I am Kaleb Lawrence</p>
+  </div>
   );
 }
 
