1f5c08004fac960b5c441fde620d737f2ec683a1
KalebLawrence <ksl00007@mix.wvu.edu>
2024-03-03 14:10:32 -0500

    Commit for week8

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import React from 'react';
+import Navbar from './navbar'; // Import the Navbar component
+import Card from './Card'; // Import the Card component
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <h1>CS 230L</h1>
-    <h2>Section - 02</h2>
-    <p>WVU ID: 800385575</p>
-    <p>Hi, I am Kaleb Lawrence</p>
-  </div>
+      <Navbar /> {/* Render the Navbar component */}
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Card /> {/* Render the Card component */}
+          </div>
+          <div className="col">
+            <Card /> {/* Render another instance of the Card component */}
+          </div>
+          <div className="col">
+            <Card /> {/* Render another instance of the Card component */}
+          </div>
+        </div>
+      </div>
+      <h1>CS 230L</h1>
+      <h2>Section - 02</h2>
+      <p>WVU ID: 800385575</p>
+      <p>Hi, I am Kaleb Lawrence</p>
+    </div>
   );
 }
 
